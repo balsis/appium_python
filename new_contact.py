de24c7304646e107ebaeb7acfc44f7facb2bb233
artur.balsis <artur.balsis@gmail.com>
2024-05-26 13:54:56 +0300

    add new_contact.py

---
+++ new_contact.py
@@ -0,0 +1,27 @@
+import time
+
+from appium import webdriver
+from typing import Any, Dict
+from appium.options.common import AppiumOptions
+
+cap: Dict[str, Any] = {
+    "platformName": "Android",
+    "automationName": "UIAutomator2",
+    "platformVersion": "14",
+    "deviceName": "emulator-5554",
+	"appPackage": "com.google.android.contacts",
+	"appActivity": "com.google.android.apps.contacts.activities.PeopleActivity"
+}
+
+url = 'http://127.0.0.1:4723'
+
+driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(cap))
+driver.implicitly_wait(50)
+
+driver.find_element("accessibility id", "Create contact").click()
+driver.find_element("xpath", "//android.widget.EditText[@text='First name']").send_keys('Test User')
+driver.find_element("xpath", "//android.widget.EditText[@text='Phone']").send_keys('+79111234567')
+driver.find_element("xpath", "//aandroid.widget.Button[@text='Save']").click()
+
+time.sleep(4)
+driver.quit()
