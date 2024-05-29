121d9eb48a2f13ab5a45faa90292ac9398845bf5
artur.balsis <artur.balsis@gmail.com>
2024-05-29 16:22:14 +0300

    fix tap and long press

--- 9_tap_and_long_press.py
+++ 9_tap_and_long_press.py
@@ -3,36 +3,49 @@ import time
 from appium import webdriver
 from typing import Any, Dict
 from appium.options.common import AppiumOptions
+from appium.webdriver.appium_service import AppiumService
 from selenium.webdriver import ActionChains
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.common.exceptions import ElementNotVisibleException, NoSuchElementException
 
 cap: Dict[str, Any] = {
-    "platformName": "Android",
-    "automationName": "UIAutomator2",
-    "platformVersion": "13",
-    "deviceName": "emulator-5554",
-    "appPackage": "com.google.android.contacts",
-    "appActivity": "com.google.android.apps.contacts.activities.PeopleActivity"
+	"platformName": "Android",
+	"automationName": "UIAutomator2",
+	"deviceName": "emulator-5554",
+	"clearSystemFiles": "true",
+	"appPackage": "com.google.android.contacts",
+	"appActivity": "com.google.android.apps.contacts.activities.PeopleActivity"
 }
 
-url = 'http://127.0.0.1:4723'
-driver = webdriver.Remote(url, options=AppiumOptions().load_capabilities(cap))
+appium_server = AppiumService()
+appium_server.start()
+# url = 'http://127.0.0.1:4723'
+driver = webdriver.Remote('http://0.0.0.0:4723', options=AppiumOptions().load_capabilities(cap))
 
-wait = WebDriverWait(driver, 20)
+wait = WebDriverWait(driver, 10)
 el0 = wait.until(EC.presence_of_element_located(("id", "android:id/button2")))
 if el0.is_displayed():
-    el0.click()
-    el1 = wait.until(
-        EC.presence_of_element_located(("id", "com.android.permissioncontroller:id/permission_allow_button")))
-    el1.click()
+	el0.click()
+# el1 = wait.until(EC.presence_of_element_located(("id", "com.android.permissioncontroller:id/permission_allow_button")))
+# if el1.is_displayed():
+#     el1.click()
 
-el2 = wait.until(EC.presence_of_element_located(("id",
-                                                 "test")))
-el2.click()
-# coord = list(el2.values())
-# print(coord)
-# actions = ActionChains(driver)
-# actions.click_and_hold(el2)
-# actions.w3c_actions.pointer_action.pointer_down(x=coord[0], y=coord[1]).perform()
+el2 = wait.until(EC.presence_of_element_located(("xpath",
+                                                 "//android.widget.TextView[@content-desc='Test']")))
+el2_coord = [tuple(el2.location.values())]
+
+## tap
+# driver.tap(el2_coord, 50)
+# time.sleep(3)
+
+## long press
+actions = ActionChains(driver)
+actions.click_and_hold(el2)
+actions.pause(1)
+actions.perform()
+time.sleep(2)
+appium_server.stop()
+
+
+
---
+++ debug.py
@@ -0,0 +1,4 @@
+
+el2 = {'x': 349, 'y': 566}
+result = [tuple(el2.values())]
+print(result)
